dfa171de0bdb3a6dd086360ad37a97c6bbc88b5a
joneaarre <jaarre@no.ibm.com>
2022-03-21 09:56:20 +0100

    Update iam.tf

--- cloud/terraform/iam.tf
+++ cloud/terraform/iam.tf
@@ -33,3 +33,10 @@ resource "ibm_iam_authorization_policy" "mongo-kms" {
   target_resource_instance_id = ibm_resource_instance.key-protect.guid
   roles                       = ["Reader", "Authorization Delegator"]
 }
+
+resource "ibm_iam_authorization_policy" "postgres-kms" {
+  source_service_name         = "databases-for-postgresql"
+  target_service_name         = "kms"
+  target_resource_instance_id = ibm_resource_instance.key-protect.guid
+  roles                       = ["Reader", "Authorization Delegator"]
+}
