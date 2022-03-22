a9da8c4907a209a80f7ccbf79fd12568e19a2a45
joneaarre <jaarre@no.ibm.com>
2022-03-22 10:27:57 +0100

    Update mongo.tf

--- cloud/terraform/mongo.tf
+++ cloud/terraform/mongo.tf
@@ -64,7 +64,7 @@ resource "ibm_is_virtual_endpoint_gateway" "vpe_mongo" {
     for_each = { for subnet in ibm_is_subnet.subnet : subnet.id => subnet }
     content {
       subnet = ips.key
-      name   = "${ips.value.name}-ip"
+      name   = "${ips.value.name}-ip-m"
     }
   }
 
